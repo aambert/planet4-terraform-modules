1c47c0ed769037cd6d959f078cbed08f981b129d
Ray Walker <raymond.walker@greenpeace.org>
2018-05-04 16:10:10 +1000

    Create databases in user module

--- cloudsql/user/main.tf
+++ cloudsql/user/main.tf
@@ -5,9 +5,26 @@ terraform {
 provider "google" {}
 
 # User configuration
-resource "google_sql_user" "users" {
+resource "google_sql_user" "user" {
   name     = "${var.cloudsql_username}"
-  instance = "${google_sql_database_instance.cloudsql_master.name}"
+  instance = "${var.cloudsql_instance}"
+  project  = "${var.project}"
   host     = "${var.cloudsql_userhost}"
   password = "${var.cloudsql_userpass}"
+}
+
+resource "google_sql_database" "production" {
+  name      = "${var.cloudsql_username}_${var.database_prefix}_production"
+  instance  = "${var.cloudsql_instance}"
+  project  = "${var.project}"
+  charset   = "${var.database_charset}"
+  collation = "${var.database_collation}"
+}
+
+resource "google_sql_database" "staging" {
+  name      = "${var.cloudsql_username}_${var.database_prefix}_staging"
+  instance  = "${var.cloudsql_instance}"
+  project  = "${var.project}"
+  charset   = "${var.database_charset}"
+  collation = "${var.database_collation}"
 }
--- cloudsql/user/variables.tf
+++ cloudsql/user/variables.tf
@@ -1,7 +1,23 @@
+variable "cloudsql_instance" {}
+
+variable "project" {}
+
 variable "cloudsql_username" {}
 
 variable "cloudsql_userpass" {}
 
 variable "cloudsql_userhost" {
   default = "%"
+}
+
+variable "database_prefix" {
+  default = "app"
+}
+
+variable "database_charset" {
+  default = "utf8mb4"
+}
+
+variable "database_collation" {
+  default = "utf8mb4_unicode_ci"
 }
