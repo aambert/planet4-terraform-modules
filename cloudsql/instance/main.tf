0ea1786d5bc4a9e8719e6cfeacdb60b61e7b80b3
Ray Walker <raymond.walker@greenpeace.org>
2018-05-04 13:01:16 +1000

    CloudSQL instance

---
+++ cloudsql/instance/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  backend "gcs" {}
+}
+
+provider "google" {}
+
+resource "google_sql_database_instance" "cloudsql_master" {
+  name = "${var.cloudsql_master_name}"
+  region = "${var.gcp_region}"
+
+  database_version = "${var.cloudsql_version}"
+
+  settings {
+    tier      = "${var.cloudsql_tier}"
+    disk_type = "${var.disk_type}"
+
+    location_preference {
+      zone = "${var.master_location_preference_zone}"
+    }
+
+    backup_configuration {
+      enabled            = "${var.backup_enabled}"
+      start_time         = "${var.backup_start_time}"
+      binary_log_enabled = true
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+
+    database_flags {
+      name  = "slow_query_log"
+      value = "on"
+    }
+
+    database_flags {
+      name  = "character_set_server"
+      value = "${var.database_charset}"
+    }
+  }
+  
+}
