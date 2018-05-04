c25becde9fb7e7c4ccb6d92e5effd7c392ff0f67
Ray Walker <raymond.walker@greenpeace.org>
2018-05-04 16:17:17 +1000

    Bugfix: Remove outputs from gke main.tf

--- gke/main.tf
+++ gke/main.tf
@@ -66,19 +66,3 @@ resource "google_container_node_pool" "default_pool" {
     max_node_count = "${var.max_node_count}"
   }
 }
-
-output "kubernetes_api_endpoint" {
-  value = "${google_container_cluster.kubernetes_cluster.endpoint}"
-}
-
-output "client_certificate" {
-  value = "${google_container_cluster.kubernetes_cluster.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.kubernetes_cluster.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.kubernetes_cluster.master_auth.0.cluster_ca_certificate}"
-}
